14e1402410f5cbf4d2054b49fce41b13b309440e
victor <victor@ubilogix.com>
2019-04-03 17:40:53 -0700

    timeout extended

--- main.go
+++ main.go
@@ -6,8 +6,6 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-var done = make(chan bool)
-
 func main() {
 	bot, err := tgbotapi.NewBotAPI("625172392:AAGTznFxi22M4m1HrAxJyRo_axd9FLmGcNk")
 	if err != nil {
@@ -19,54 +17,45 @@ func main() {
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
+	u.Timeout = ((60 * 60) * 24) * 7
 
 	updates, err := bot.GetUpdatesChan(u)
-	go func() {
-		for update := range updates {
-			if update.Message == nil { // ignore any non-Message Updates
-				continue
-			}
 
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-			if update.Message.Text == "hi" {
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "hi")
-				bot.Send(msg)
-				continue
-			}
-
-			//really nigga?.... asi se para el bot(por el momento) y que?!..
-			if update.Message.Text == "pararbot" {
-				done <- true
-				continue
-			}
-
-			if update.Message.Sticker != nil {
-				log.Println("sticker is comming")
-				msg := tgbotapi.NewStickerShare(update.Message.Chat.ID, update.Message.Sticker.FileID)
-				bot.Send(msg)
-				continue
-			}
-			// msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			// msg.ReplyToMessageID = update.Message.MessageID
-			if update.Message.IsCommand() {
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-				switch update.Message.Command() {
-				case "help":
-					msg.Text = "type /sayhi or /status."
-				case "sayhi":
-					msg.Text = "Hi :)"
-				case "status":
-					msg.Text = "I'm ok."
-				default:
-					msg.Text = "I don't know that command"
-				}
-				bot.Send(msg)
-			}
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
 		}
-	}()
 
-	<-done
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		if update.Message.Text == "hi" {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "hi")
+			bot.Send(msg)
+			continue
+		}
+
+		if update.Message.Sticker != nil {
+			log.Println("sticker is comming")
+			msg := tgbotapi.NewStickerShare(update.Message.Chat.ID, update.Message.Sticker.FileID)
+			bot.Send(msg)
+			continue
+		}
+		// msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		// msg.ReplyToMessageID = update.Message.MessageID
+		if update.Message.IsCommand() {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+			switch update.Message.Command() {
+			case "help":
+				msg.Text = "type /sayhi or /status."
+			case "sayhi":
+				msg.Text = "Hi :)"
+			case "status":
+				msg.Text = "I'm ok."
+			default:
+				msg.Text = "I don't know that command"
+			}
+			bot.Send(msg)
+		}
+	}
 
 }
